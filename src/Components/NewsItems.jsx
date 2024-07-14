15626e61629b8284d26722e9e135c117ee966f7d
Yashraj_K <yashrajkamble7727@gamil.com>
2024-07-14 15:35:59 +0530

    read more add in newsitems

--- src/Components/NewsItems.jsx
+++ src/Components/NewsItems.jsx
@@ -14,17 +14,20 @@ function NewsItems({ title, description, src, url }) {
       />
 
       <div className="card-body">
-        <h5 className="card-title">{title.slice(0, 31)}</h5>
+        <h5 className="card-title">
+          {title.length > 30 ? `${title.slice(0, 30)}...` : title}
+        </h5>
         <p className="card-text">
           {description
-            ? description.slice(0, 90)
+            ? description.length > 90
+              ? `${description.slice(0, 90)}...`
+              : description
             : "News is a report of a current event.it is information about something that has just happened. "}
         </p>
         <a href={url} className="btn btn-primary">
           Read More
         </a>
       </div>
-      
     </div>
   );
 }
