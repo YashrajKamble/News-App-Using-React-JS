0439a0d1bf19e3aadd4bd775d2b769269bc4b929
Yashraj_K <yashrajkamble7727@gamil.com>
2024-07-14 14:13:07 +0530

    update the newsItems.jsx file

--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -1,16 +1,18 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import NewsItems from "./NewsItems";
 
 function NewsBoard() {
   const [articles, setArticles] = useState([]);
+
   useEffect(() => {
-    let url = `GET https://newsapi.org/v2/everything?q=bitcoin&apiKey=${
+    const url = `https://newsapi.org/v2/everything?q=bitcoin&apiKey=${
       import.meta.env.VITE_API_KEY
     }`;
+
     fetch(url)
       .then((response) => response.json())
-      .then((data) => setArticles(data.articles));
+      .then((data) => setArticles(data.articles))
+      .catch((error) => console.error("Error fetching data:", error));
   }, []);
 
   return (
@@ -19,17 +21,15 @@ function NewsBoard() {
         Latest <span className="badge text-light bg-danger fs-4">News </span>
       </h2>
 
-      {articles.map((news, index) => {
-        return (
-          <NewsItems
-            key={index}
-            title={news.title}
-            description={news.description}
-            src={news.urlToImage}
-            url={news.url}
-          />
-        );
-      })}
+      {articles.map((news, index) => (
+        <NewsItems
+          key={index}
+          title={news.title}
+          description={news.description}
+          src={news.urlToImage}
+          url={news.url}
+        />
+      ))}
     </div>
   );
 }
--- src/Components/NewsItems.jsx
+++ src/Components/NewsItems.jsx
@@ -1,15 +1,12 @@
-function NewsItems() {
+function NewsItems({ title, description, src, url }) {
   return (
-    <div className="card" style="width: 18rem;">
-      <img src="..." className="card-img-top" alt="..." />
+    <div className="card" style={{ maxWidth: "345px" }}>
+      <img src={src} className="card-img-top" alt="..." />
       <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card&aposs content.
-        </p>
-        <a href="/" className="btn btn-primary">
-          Go somewhere
+        <h5 className="card-title">{title}</h5>
+        <p className="card-text">{description}</p>
+        <a href={url} className="btn btn-primary">
+          Read More
         </a>
       </div>
     </div>
